d979c2b2991d7efabfefee5971127a9d8e8e513c
Aldovani <aldovanihcosta@gmail.com>
2023-11-22 16:32:54 -0300

    update

--- web/next.config.js
+++ web/next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    API_URL: 'http://localhost:8080/v1/',
+    LOCAL_STORAGE_KEY: 'ibico:dev',
+  },
+}
 
 module.exports = nextConfig
