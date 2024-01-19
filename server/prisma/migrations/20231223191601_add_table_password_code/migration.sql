919a88b36a443c6bd4ae9382417d8a9cf559c359
Aldovani <aldovanihcosta@gmail.com>
2024-01-18 21:51:03 -0300

    remove sql lite

--- server/prisma/migrations/20231223191601_add_table_password_code/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "password_codes" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "code" TEXT NOT NULL,
-    "expiresAt" DATETIME NOT NULL,
-    "createdAt" DATETIME NOT NULL,
-    "userId" TEXT NOT NULL,
-    CONSTRAINT "password_codes_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
-);
--- server/prisma/migrations/20231223202536_add_table_password_reset/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "password_resets" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "expiresAt" DATETIME NOT NULL,
-    "createdAt" DATETIME NOT NULL,
-    "used" BOOLEAN NOT NULL,
-    "passwordCodeId" TEXT NOT NULL,
-    CONSTRAINT "password_resets_passwordCodeId_fkey" FOREIGN KEY ("passwordCodeId") REFERENCES "password_codes" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
-);
